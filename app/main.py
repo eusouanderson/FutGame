a89f164f119d3ad5e16fa6b270eb806b061bfeb4
Anderson Rodrigues <eusouanderson@outlook.com>
2025-05-08 16:03:13 -0300

    updated

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Server is runing Ultimo teste!"}
+    return {"message": "Server is runing Ultimo por hoje!"}
 
 @app.post("/webhook")
 async def webhook(request: Request):
