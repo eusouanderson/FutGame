118c1c266070546a189f89171e5e51bbfc1c89ed
Anderson Rodrigues <eusouanderson@outlook.com>
2025-05-08 13:48:49 -0300

    updated

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Server is Ok funciona Sera HOJE?!"}
+    return {"message": "Server is Ok funciona Sera HOJE MESMO?!"}
 
 @app.post("/webhook")
 async def webhook(request: Request):
