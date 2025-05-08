77f586f8605ffade5fd3a208740f1e2b816513b2
Anderson Rodrigues <eusouanderson@outlook.com>
2025-05-08 18:04:33 -0300

    updated

--- app/main.py
+++ app/main.py
@@ -20,24 +20,11 @@ async def webhook(request: Request):
         print("⏳ Aguardando para garantir que as alterações sejam aplicadas...")
         time.sleep(3)
 
-        print("♻️ Reiniciando o servidor FastAPI com o script de reinício...")
-
-        restart_script_path = "/root/projetos/FutGame/restart.sh"
-        result = subprocess.run(['bash', restart_script_path], text=True, capture_output=True)
-
-
-        if result.returncode != 0:
-            print(f"❌ Erro ao reiniciar servidor. Código de erro: {result.returncode}")
-            print(f"Detalhes do erro: {result.stderr}")
-            return Response(content="Falha ao reiniciar o servidor", status_code=500)
-
-        print("✅ Servidor reiniciado com sucesso.")
-
-        return Response(content="Success and restarted!", status_code=200)
-
     except subprocess.CalledProcessError as e:
         print("❌ Erro ao executar git pull:\n", e.output.decode())
         return Response(content="Erro ao atualizar o código", status_code=500)
     except Exception as e:
         print("❌ Erro desconhecido:\n", str(e))
         return Response(content="Erro desconhecido", status_code=500)
+    print("✅ Atualização concluída com sucesso.")
+    return Response(content="Atualização concluída com sucesso", status_code=200)
